8b36bd33633d8b29ef318fe1e776f867e11f75fb
Jerry Muir <jerry.muir@outlook.com>
2021-02-15 17:38:35 -0500

    Add files via upload

---
+++ Firmware/CSPB/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 6/13/2020 3:25:40 PM
+ * Author : jerry
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
